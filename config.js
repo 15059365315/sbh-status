0ce4e065bbf7d3d1dc4c69753f66acfb6661a380
司白画 <83536228+15059365315@users.noreply.github.com>
2022-08-16 21:54:08 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '司白画の状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792432655-a68126826c6fe6cc9509797b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '首页',
+      url: 'https://sbh.rth1.one/'
+    },
+    {
+      text: '博客',
+      url: 'https://sibaihua.top'
+    },
+    {
+      text: '友人帐',
+      url: 'https://sibaihua.top/friends.html'
+    },
+  ],
+};
